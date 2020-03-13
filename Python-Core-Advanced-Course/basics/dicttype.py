b3a1e61475fc93ae1d259d86358ff1a2f4f2ba75
AlainDlcTVOP <alain_dlc@outlook.com>
2020-03-13 21:59:43 +0100

    dictonary

---
+++ Python-Core-Advanced-Course/basics/dicttype.py
@@ -0,0 +1,18 @@
+# Key : Value
+dict1 = {1:"King", 2:"Solhed", 3:"Sanita"}
+
+print(dict1)
+
+print(dict1.items())
+
+k = dict1.keys()
+for i in k:print(i)
+
+v=dict1.values()
+for i in v:print(i)
+
+print(dict1[3])
+
+del dict1[1]
+print(dict1)
+
