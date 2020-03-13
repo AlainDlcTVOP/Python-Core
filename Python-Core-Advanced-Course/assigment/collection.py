17f67f48a39f5d1660e088ab2ead89bf5aad1ab0
AlainDlcTVOP <alain_dlc@outlook.com>
2020-03-13 22:11:15 +0100

    assigment collection

---
+++ Python-Core-Advanced-Course/assigment/collection.py
@@ -0,0 +1,12 @@
+#List of countries
+lst= ["Spain","Portugal","Norway"]
+print(lst)
+#Add an country at the end
+lst.append("Mexico")
+print(lst)
+#Remove by index
+lst.remove("Mexico")
+print(lst)
+#Add a country in the middle
+lst.insert( 1,"Brazil")
+print(lst)
