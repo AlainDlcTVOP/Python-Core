e84293bc7bbb5ea55c719272ac610509e3a2ad39
AlainDlcTVOP <alain_dlc@outlook.com>
2020-03-13 21:36:04 +0100

    tuple

---
+++ Python-Core-Advanced-Course/basics/tupletype.py
@@ -0,0 +1,13 @@
+tpl=(40,50,40,'kungen')
+print(tpl)
+print(tpl[3])
+print(tpl*3)
+print(tpl.count(40))# how many 
+print(tpl.index('kungen'))
+
+lst=[67,34,"XYZ"]
+print(type(lst))
+#convert list to tuple
+tpl1=tuple(lst)
+print(type(tpl1))
+print(tpl1)
