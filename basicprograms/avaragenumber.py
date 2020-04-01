2ac99be56e5cc3dcbc7edbe7520b444fe5289516
AlainDlcTVOP <alain_dlc@outlook.com>
2020-04-01 18:59:04 +0200

    average

---
+++ basicprograms/avaragenumber.py
@@ -0,0 +1,4 @@
+# Calculate 3 average number
+a, b, c = [int(x) for x in input("Enter three integer number (use space)").split()]
+average = (a + b + c) / 3;
+print("Average of the three numbers is:", average)
