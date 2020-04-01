264d9a66a870e26f4eee16f13513784087f8960b
AlainDlcTVOP <alain_dlc@outlook.com>
2020-04-01 18:51:22 +0200

    20200401

---
+++ basicprograms/studentdetils.py
@@ -0,0 +1,5 @@
+studentid = int (input("Enter Student Id\n"))
+name = input("Enter Student name\n")
+marks = float(input("Enter marks scores\n"))
+
+print("Id:",studentid,"Name:",name,"Marks:",marks)
